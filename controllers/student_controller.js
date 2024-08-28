3ef333053cf4f17d97cee78a7daf9f01f0d6f412
buhleqampi <buhle3857@gmail.com>
2024-08-28 15:59:28 +0200

    added config folder & students' controller functions

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -1,47 +1,69 @@
 const Student = require('../models/student_model');
+const mongoose = require('mongoose');
 
-
-router.get('/', async (req, res) => {
+exports.getAllStudents =  async (req, res) => {
   try {
-    const allData = await Data.find();
-    res.json(allData);
+    const students = await Student.find();
+    res.send(students);
+  } catch (error) {
+    res.status(500).send({ message:"Could not fetch student" , error:error.message });
+  }
+};
+
+exports.getOneStudent =  async (req, res) => {
+  try {
+    const {id} = req.params
+    const student = await Student.findById(id);
+    if(!student){
+        return res.status(404).send({ message: "Student not found"});
+    }
+    res.json(student);
+  } catch (error) {
+    res.status(500).send({ message:"Could not fetch student" , error:error.message });
+  }
+};
+
+
+exports.createStudent =  async (req, res) => {
+  const newStudent = new Student(req.body);
+  try {
+    const savedStudent = await newStudent.save();
+    res.json(savedStudent);
   } catch (error) {
     res.json({ message: error.message });
   }
-});
+};
 
-// POST new data
-router.post('/', async (req, res) => {
-  const newData = new Data(req.body);
+exports.updateStudent = async (req, res) => {
   try {
-    const savedData = await newData.save();
-    res.json(savedData);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-});
-
-// PUT update data
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedData = await Data.updateOne(
+    const updatedStudent = await Student.updateOne(
       { _id: req.params.id },
       { $set: req.body }
     );
-    res.json(updatedData);
+    res.json(updatedStudent);
   } catch (error) {
     res.json({ message: error.message });
   }
-});
+};
 
-// DELETE data
-router.delete('/:id', async (req, res) => {
-  try {
-    const removedData = await Data.deleteOne({ _id: req.params.id });
-    res.json(removedData);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-});
+
+exports.deleteAllStudents = async (req, res) => {
+    try {
+        await Student.deleteMany({});
+        res.send({ message: "All students deleted successfully" });
+      } catch (error) {
+        res.status(500).send({ message: "Could not delete recipes", error: error.message });
+      }
+    };
+
+
+exports.deleteStudents = async (req, res) => {
+    try {
+      const removedStudent = await Student.deleteOne({ _id: req.params.id });
+      res.json(removedStudent);
+    } catch (error) {
+      res.json({ message: error.message });
+    }
+  };
 
 module.exports = router; 
