b52821789a7f2445f498a044a9c2b49d20c60194
buhleqampi <buhle3857@gmail.com>
2024-09-11 17:44:36 +0200

    attachments

--- helpers/handle_email.js
+++ helpers/handle_email.js
@@ -4,13 +4,18 @@ const { transporter } = require('../config/config');
 const handleEmail = async ( email, subject,  message , file) => {
     try {
 
-        // const fileContent = Buffer.from(file.data)
+         const fileContent = Buffer.from(file.data).toString('base64')
            
         const mailDetails = {
             from: process.env.EMAIL,
             to:email,
             subject: subject,
             html: `<b>Email: ${email}</b><br> ${message}<p></p>`,
+            attachments: [
+                {
+                    content: `${fileContent}`
+                }
+            ]
         };
         await transporter.sendMail(mailDetails);
         console.log(mailDetails)
